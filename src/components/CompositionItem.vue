504b6222f452ca5a579aaa3c44c13e56d8518659
khrystyna.stupak <khrystyna.rypetska@gmail.com>
2023-10-18 22:57:04 +0300

    add howler liberary

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -29,6 +29,7 @@
               type="text"
               class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               placeholder="Enter Song Title"
+                     @input="updateUnsavedFlag(true)"
           />
           <ErrorMessage class="text-red-600" name="modified_name"/>
         </div>
@@ -38,6 +39,7 @@
               type="text"
               class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
               placeholder="Enter Genre"
+                     @input="updateUnsavedFlag(true)"
           />
           <ErrorMessage class="text-red-600" name="genre"/>
         </div>
@@ -82,6 +84,9 @@ export default {
     removeSong: {
       type: Function,
       required: true,
+    },
+    updateUnsavedFlag: {
+      type: Function,
     }
   },
   data() {
@@ -114,6 +119,7 @@ export default {
       }
 
       this.updateSong(this.index, values);
+      this.updateUnsavedFlag(false);
 
       this.in_submission = false;
       this.alert_variant = "bg-green-500";
